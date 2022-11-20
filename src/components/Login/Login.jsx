ee017bdbeba785c1b703288d233b3e829eba11c9
Erik Persson <perssonerik7@gmail.com>
2022-11-20 19:48:26 +0100

    added icons

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { auth } from "../../firebase";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { FcGoogle } from "react-icons/fc";
+import { Slack } from "react-feather";
 import "./Login.css";
 
 export default function Login() {
   console.log(auth.currentUser);
   return (
     <div className="login">
-      <h1>Welcome to my slack clone</h1>
+      <h1>
+        Welcome to my slack clone <Slack />
+      </h1>
+
       <button
         onClick={async () => {
           const provider = new GoogleAuthProvider();
@@ -16,7 +21,9 @@ export default function Login() {
           console.log(user);
         }}
       >
-        <h3>Log in with Google</h3>
+        <h3>
+          Sign in with Google <FcGoogle className="google" />
+        </h3>
       </button>
     </div>
   );
