af308acba505ef28eece4997bc6541caf83ed367
Erik Persson <perssonerik7@gmail.com>
2022-11-24 17:56:55 +0100

    modified

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,32 +1,28 @@
-import React, {createContext, useContext, useState, useEffect} from 'react'
-import {auth} from '../firebase'
-import {onAuthStateChanged} from 'firebase/auth'
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
-const AuthContext = createContext(null)
+const AuthContext = createContext(null);
 
-
-export function useAuth(){
-  return useContext(AuthContext)
+export function useAuth() {
+  return useContext(AuthContext);
 }
 
-export default function AuthProvider({children}) {
+export default function AuthProvider({ children }) {
+  const [fetchingUser, setFetchingUser] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
 
-
-    const [fetchingUser, setFetchingUser] = useState(true)
-    const [currentUser, setCurrentUser] = useState(null)
-
-    useEffect(()=>{
-   const unsubscribe = onAuthStateChanged(auth, (user)=>{
-    setCurrentUser(user)
-    setFetchingUser(false)
-})
-return unsubscribe
-    }, [])
-
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setFetchingUser(false);
+    });
+    return unsubscribe;
+  }, []);
 
   return (
-    <AuthContext.Provider value={{currentUser, fetchingUser}}>
+    <AuthContext.Provider value={{ currentUser, fetchingUser }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
