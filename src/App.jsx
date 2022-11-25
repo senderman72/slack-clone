2393837d61b29e446a13f744745fa0ec37df7469
Erik Persson <perssonerik7@gmail.com>
2022-11-25 14:13:02 +0100

    added new route

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
 
         <Route element={<Layout />}>
