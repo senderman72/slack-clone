24b3cbaf23cb0b03e47170a72196c97a556ff94b
Erik Persson <perssonerik7@gmail.com>
2025-03-07 16:36:45 +0100

    uppdaterade appen och lade till en logga ut knapp

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -6,16 +6,22 @@ import { ChevronLeft } from "react-feather";
 import { Link, useParams } from "react-router-dom";
 
 export default function TopNav() {
-  const { currentUser } = useAuth();
+  const { currentUser, logOut } = useAuth();
   const { id } = useParams();
 
   if (!currentUser) {
     return <div className="loading"></div>;
   }
 
+  const handleLogOut = () => {
+    logOut();
+  };
+
   return (
     <div className="top-nav">
-      {" "}
+      <button className="Logout-btn" onClick={handleLogOut}>
+        Logga ut
+      </button>{" "}
       <div>
         {" "}
         {id && (
@@ -24,6 +30,7 @@ export default function TopNav() {
           </Link>
         )}
       </div>
+      <div></div>
       <div className="profile">
         <img src={currentUser.photoURL} />
         <h3>{currentUser.displayName}</h3>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
 import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = createContext(null);
@@ -20,8 +21,16 @@ export default function AuthProvider({ children }) {
     return unsubscribe;
   }, []);
 
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error("Failed to log out", error);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ currentUser, fetchingUser }}>
+    <AuthContext.Provider value={{ currentUser, fetchingUser, logOut }}>
       {children}
     </AuthContext.Provider>
   );
